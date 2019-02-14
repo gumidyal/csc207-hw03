6eb27b98cb318f0bdf2c8d8ab02fe4abe4432210
Ally-R <ally.rogers.mail@gmail.com>
2019-02-14 11:08:57 -0600

    More documentation for Part D

--- src/com/mcfarevee/shopping/Cart.java
+++ src/com/mcfarevee/shopping/Cart.java
@@ -9,38 +9,66 @@ public class Cart {
   // | Fields |
   // +--------+
   
+  /**
+   * The cart of Item types
+   */
   ArrayList<Item> cart;
+  
+  /**
+   * The name of the cart
+   */
   String name;
+  
+  /**
+   * Counter for the total number of packages in the cart
+   */
   int numPackages;
-  //int numItems;
+  
+  /**
+   * Counter for the total price of all items in the cart
+   */
   int totPrice;
+  
+  /**
+   * Counter for the total weight of all items in the cart
+   */
   int totWeight;
 
   // +--------------+------------------------------------------------
   // | Constructors |
   // +--------------+
 
+  /**
+   * Create a cart of the given name to hold type Item
+   */
   public Cart(String name) {
     this.name = name;
     this.numPackages = 0;
-    //this.numItems = 0;
     this.totPrice = 0;
     this.totWeight = 0;
-  }
+  } // Cart(String name)
 
   // +---------+-----------------------------------------------------
   // | Methods |
   // +---------+
 
+  /**
+   * Add given item to cart
+   */
   public void addItem(Item item) {
 
   }
 
+  /**
+   * Get the number of things in the cart (e.g. Packages, BulkItems, Non-Foods)
+   */
   public int numThings() {
 
   }
 
-
+  /**
+   * Get the total number of entries in the cart
+   */
   public int numEntries() {
 
 
